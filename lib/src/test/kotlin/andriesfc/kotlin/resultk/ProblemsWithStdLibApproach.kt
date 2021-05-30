530da5f9a9e591d4802126917f217b9f6009a1d2
Andries <andries.spies@gmail.com>
2021-05-30 13:48:45 +0200

    Comment out some test cases (need to do them later)

--- lib/src/test/kotlin/andriesfc/kotlin/resultk/ProblemsWithStdLibApproach.kt
+++ lib/src/test/kotlin/andriesfc/kotlin/resultk/ProblemsWithStdLibApproach.kt
@@ -3,26 +3,30 @@ package andriesfc.kotlin.resultk
 import org.junit.jupiter.api.Test
 
 class ProblemsWithStdLibApproach {
-
+/*
+    // todo
     @Test
     fun catch_the_world_deal_with_the_world() {
         TODO()
     }
 
+    // todo
     @Test
     fun hiding_low_level_error_behind_getOrNull() {
         TODO()
     }
 
+    // todo
     @Test
     fun tedious_to_use_correctly() {
         TODO()
     }
 
+    // todo
     @Test
     fun easy_to_use_incorrectly_aka_how_do_i_use_it_actually() {
         TODO()
-    }
+    }*/
 
 }
 
