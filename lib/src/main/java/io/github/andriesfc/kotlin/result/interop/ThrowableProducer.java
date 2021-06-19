975fed7c7229ad3564aeb9fff5854d8c3372e7b2
AndriesFC <andriesfc@gmail.com>
2021-06-19 11:15:13 +0200

    Added some javadocs.

--- lib/src/main/java/io/github/andriesfc/kotlin/result/interop/ThrowableProducer.java
+++ lib/src/main/java/io/github/andriesfc/kotlin/result/interop/ThrowableProducer.java
@@ -2,6 +2,19 @@ package io.github.andriesfc.kotlin.result.interop;
 
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * A functional interface which models a producer function which throws any exception. It is up the
+ * caller to deal with any possible exception being thrown.
+ *
+ * @param <T> The type parameter of instance this function should return.
+ *
+ */
+@FunctionalInterface
 public interface ThrowableProducer<T> {
+    /**
+     * Produces a <em>non nullable</em> instance of <code>T</code>
+     * @return A non nullable instance
+     * @throws Throwable If any exception within the function implementation throws an exception.
+     */
     @NotNull T produce() throws Throwable;
 }
